64a63710fefd349d0a32f82b0c6e0b45dcb664b2
Andrey Sysoev <andrey@sysoev.ru>
2020-09-24 11:47:23 +0300

    Added babel configuretion in webpack.comfig

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
-const test = 3;
-let someStr = 'Изменил, сохранил, собрал';
+const numbers = [2, 3, 5];
+const doubledNumbers = numbers.map(number => number * 2); // Стрелочная функция. Не запнётся ли на ней Internet Explorer
+
+console.log(doubledNumbers); // 4, 6, 10
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const { loader } = require('mini-css-extract-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  module: {
+    rules: [{
+      test: /\.js$/,
+      use: { loader: 'babel-loader' },
+      exclude: /node-modules/
+    }
+    ]
+  }
+}
