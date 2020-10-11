9ae0897869a1e7848958d39d70f667b144c5a8c9
Andrey Sysoev <andrey@sysoev.ru>
2020-10-11 21:17:47 +0300

    Deploy works correctly

--- webpack.config.js
+++ webpack.config.js
@@ -1,98 +1,96 @@
-const path = require('path');
-const webpack = require('webpack');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackMd5Hash = require('webpack-md5-hash');
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const WebpackMd5Hash = require("webpack-md5-hash");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const ImageminPlugin = require('imagemin-webpack-plugin').default;
-const FontminPlugin = require('fontmin-webpack');
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const ImageminPlugin = require("imagemin-webpack-plugin").default;
+const FontminPlugin = require("fontmin-webpack");
 
-const isDev = process.env.NODE_ENV === 'development';
+const isDev = process.env.NODE_ENV === "development";
 
 module.exports = {
   entry: {
-    index: './src/index.js',
-    saved: './src/saved-articles/index.js'
+    main: "./src/main.js",
+    articles: "./src/saved-articles/articles.js",
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: '[name].[chunkhash].js'
+    path: path.resolve(__dirname, "dist"),
+    filename: "./JS/[name].[chunkhash].js",
   },
   module: {
     rules: [
       {
         test: /\.js$/,
+        use: { loader: "babel-loader" },
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
-        test: /\.css$/i,
-        use: [
-          (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-          {
-            loader: 'css-loader',
-            options: {
-              importLoaders: 2
-            }
-          },
-          'postcss-loader'
-        ]
       },
       {
         test: /\.(png|jpe?g|gif|ico|svg)$/,
         use: [
-          'file-loader?name=./src/images/[name].[ext]',
+          "file-loader?name=./images/[name].[ext]",
           {
-            loader: 'image-webpack-loader',
-            options: {}
+            loader: "image-webpack-loader",
+            options: {},
           },
-        ]
+        ],
       },
       {
         test: /\.(eot|ttf|woff|woff2)$/,
-        loader: 'file-loader?name=./src/vendor/[name].[ext]'
+        loader: "file-loader?name=./vendor/[name].[ext]",
       },
-    ]
+      {
+        test: /\.css$/i,
+        use: [
+          isDev ? "style-loader" : MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+            options: {
+              importLoaders: 2,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'index.[contenthash].css',
+      filename: "./[name].[contenthash].css",
     }),
     new HtmlWebpackPlugin({
       inject: false,
-      template: './src/index.html',
-      filename: 'index.html',
-      chunks: ['index']
+      template: "./src/index.html",
+      filename: "index.html",
+      chunks: ["main"],
     }),
     new HtmlWebpackPlugin({
       inject: false,
-      template: './src/saved.html',
-      filename: 'saved.html',
-      chunks: ['saved']
+      template: "./src/saved-articles/articles.html",
+      filename: "./articles.html",
+      chunks: ["articles"],
     }),
     new WebpackMd5Hash(),
     new webpack.DefinePlugin({
-      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      NODE_ENV: JSON.stringify(process.env.NODE_ENV),
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
-      cssProcessor: require('cssnano'),
+      cssProcessor: require("cssnano"),
       cssProcessorPluginOptions: {
-        preset: ['default'],
+        preset: ["default"],
       },
-      canPrint: true
+      canPrint: true,
     }),
     new ImageminPlugin({
-      disable: process.env.NODE_ENV !== 'production',
+      disable: process.env.NODE_ENV !== "production",
       pngquant: {
-        quality: '95-100'
-      }
+        quality: "95-100",
+      },
     }),
     new FontminPlugin({
       autodetect: true,
-      glyphs: ['\uf0c8']
-    })
-  ]
+      glyphs: ["\uf0c8"],
+    }),
+  ],
 };
